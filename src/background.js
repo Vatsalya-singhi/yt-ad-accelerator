c52f7daee2291f113d9f5a8fd06055c2929b510e
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2024-05-20 17:02:29 +0200

    tab based conter update

--- src/background.js
+++ src/background.js
@@ -1,17 +1,20 @@
 var tabList = [];
 var adSkipCount = 0;
+var tabAdSkipCounts = {};
 
-// Function to update badge text
-function updateBadgeText() {
-    chrome.action.setBadgeText({ text: adSkipCount.toString() });
-    chrome.action.setBadgeTextColor({ color: "#FFFFFF" });
-    chrome.action.setBadgeBackgroundColor({ color: '#353935' });
+// Function to update badge text for a specific tab
+function updateBadgeText(tabId) {
+    const adSkipCount = tabAdSkipCounts[tabId] || 0;
+    chrome.action.setBadgeText({ tabId: tabId, text: adSkipCount.toString() });
+    chrome.action.setBadgeTextColor({ tabId: tabId, color: "#FFFFFF" });
+    chrome.action.setBadgeBackgroundColor({ tabId: tabId, color: '#353935' });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "incrementAdSkipCount") {
-        adSkipCount++;
-        updateBadgeText();
+        const tabId = sender.tab.id;
+        tabAdSkipCounts[tabId] = (tabAdSkipCounts[tabId] || 0) + 1;
+        updateBadgeText(tabId);
     }
 });
 
@@ -29,6 +32,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             }).then(() => {
                 console.info(`INJECTED THE FOREGROUND SCRIPT for tab ${tabId}`);
                 tabList.push(tabId);
+                tabAdSkipCounts[tabId] = 0;
+                updateBadgeText(tabId);
             }).catch((err) => {
                 console.error(`Failed to inject script: ${err.message}`);
             });
@@ -40,6 +45,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
     if (tabList.includes(tabId)) {
         tabList = tabList.filter(x => x !== tabId);
+        delete tabAdSkipCounts[tabId];
         chrome.tabs.sendMessage(tabId, { action: "disconnectObserver" });
         console.info(`script removed for tab ${tabId}`);
     }
