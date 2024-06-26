c35a8b84709b7a4f1f812ea46585369fcbd29361
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2024-06-26 20:55:36 +0200

    background script to content script update

--- src/content2.js
+++ src/content2.js
@@ -1,97 +1,72 @@
 (() => {
-    let observer = null;
-    let observer2 = null;
+
+    /**
+    * GLOBAL VARIABLES
+    */
+
     const playbackRate = 16;
+    let obs1 = null;
+    let obs2 = null;
+    let obs3 = null;
+    let obs4 = null;
 
     /**
     * MAIN FUNCTIONS
     */
 
-    const srcCode = () => {
+    const sourceCode = () => {
+        // check if exist
+        if (obs1) return;
+        // check for element
+        const condition1 = getElementByXpath('/html/body/ytd-app');
+        if (!condition1) return;
 
-        const condition2 = getElementByXpath('/html/body/ytd-app');
-        if (!condition2) return;
+        obs1 = new MutationObserver(() => {
+            // check if exist
+            if (obs2) return;
+            // check for element
+            const condition2 = getElementByXpath('/html/body/ytd-app/div[1]/ytd-page-manager');
+            if (!condition2) return;
 
-        const obs2 = new MutationObserver(() => {
-            const condition3 = getElementByXpath('/html/body/ytd-app/div[1]/ytd-page-manager');
-            if (!condition3) return;
+            obs2 = new MutationObserver(() => {
+                // check if exist
+                if (obs3) return;
+                // check for element
+                const condition3 = getElementByXpath('//*[@id="page-manager"]/ytd-watch-flexy');
+                if (!condition3) return;
 
-            const obs3 = new MutationObserver(() => {
-                const condition4 = getElementByXpath('//*[@id="page-manager"]/ytd-watch-flexy');
-                if (!condition4) return;
+                obs3 = new MutationObserver(() => {
+                    // check if exist
+                    if (obs4) return;
+                    // check for element
+                    const condition4 = getElementByXpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player');
+                    if (!condition4) return;
 
-                const obs4 = new MutationObserver(() => {
-                    const condition5 = getElementByXpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player');
-                    if (!condition5) return;
-
-                    const obs5 = new MutationObserver(() => {
-                        // obs5.disconnect();
-                        // obs4.disconnect();
-                        // obs3.disconnect();
-                        obs2.disconnect();
-
-                        if (!observer) {
-                            skipBtnClick();
-                            adVideoManipulation();
-                        }
-                        if (!observer2) {
-                            enforcementListener();
-                        }
+                    obs4 = new MutationObserver(() => {
+                        skipBtnClick();
+                        adVideoManipulation();
+                        closeEnforcementMessage();
                     })
-
-                    obs5.observe(condition5, {
+                    obs4.observe(condition4, {
                         childList: true,
                         subtree: true,
                     });
-
                 });
-
-                obs4.observe(condition4, {
+                obs3.observe(condition3, {
                     childList: true,
                     subtree: true,
                 });
-
             })
-
-            obs3.observe(condition3, {
-                childList: true,
-                // subtree: true,
-            });
-
-        })
-
-        obs2.observe(condition2, {
-            childList: true,
-            // subtree: true,
-        });
-
-    }
-
-
-    const enforcementListener = () => {
-        const e = getElementByXpath('/html/body/ytd-app');
-        if (!e) return;
-
-        try {
-            observer2 = new MutationObserver(() => {
-                closeEnforcementMessage();
-            });
-            observer2.observe(e, {
+            obs2.observe(condition2, {
                 childList: true,
                 subtree: true,
-                // attributes: true,
-                // attributeFilter: ['class', 'style'],
-                // characterData: true,
             });
-        } catch (err) {
-            console.error(err);
-        }
+        })
+        obs1.observe(condition1, {
+            childList: true,
+            subtree: true,
+        });
     }
-
-    /**
-    * MAIN FUNCTIONS
-    */
-
 
     /**
     * HELPER FUNCTIONS
@@ -134,37 +109,25 @@
     }
 
     const adVideoManipulation = () => {
-        try {
-            const video = getElementByXpath('//*[@id="movie_player" and contains(@class, "ad-showing")]/div[1]/video');
-            if (video) {
-                video.volume = 0;
-                video.muted = true;
-                video.playbackRate = playbackRate;
-            }
-        } catch (err) {
-            console.error(err);
-        }
+        const videoElement = getElementByXpath('//*[@id="movie_player" and contains(@class, "ad-showing")]/div[1]/video');
+        if (!videoElement) return;
+        videoElement.volume = 0;
+        videoElement.muted = true;
+        videoElement.playbackRate = playbackRate;
     }
 
     const closeEnforcementMessage = () => {
-        try {
-            const elementXPath = '//*[@id="container" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="header" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="dismiss-button" and contains(@class, "ytd-enforcement-message-view-model")]/button-view-model/button';
-            const adElement = getElementByXpath(elementXPath);
-            if (adElement) {
-                adElement.click();
-                console.info('enforcement button click by XPath successful');
-            }
-        } catch (err) {
-            console.error(err);
-        }
+        const adElement = getElementByXpath('//*[@id="container" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="header" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="dismiss-button" and contains(@class, "ytd-enforcement-message-view-model")]/button-view-model/button');
+        if (!adElement) return;
+        adElement.click();
+        console.info('enforcement button click by XPath successful');
     }
 
     /**
-    * HELPER FUNCTIONS
+    * INIT FUNCTIONS
     */
 
-
     // Initial setup and check
-    window.addEventListener("load", () => { srcCode(); })
+    window.addEventListener("load", () => { sourceCode(); })
 
 })();
