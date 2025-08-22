dc5a637b1ca6a5d5065ecbd3a023b315400f446d
Vatsalya-singhi <vatsalya.singhi4316@gmail.com>
2025-08-22 19:20:31 +0200

    slight update

--- src/content.js
+++ src/content.js
@@ -1,96 +1,107 @@
 (() => {
-    let observer = null;
-    let observer2 = null;
-    let adSkipCount = 0;
+
+    /**
+    * GLOBAL VARIABLES
+    */
+
     const playbackRate = 16;
-
-    // Initial setup and check
-    setTimeout(() => {
-        try {
-            adVideoManipulation();
-            skipBtnClick();
-            enforcementListener();
-        } catch (err) {
-            console.error(err);
-        }
-    });
-
-    // Ensure observers are set up after initial delay
-    setTimeout(() => {
-        if (!observer) {
-            main();
-        }
-        if (!observer2) {
-            enforcementListener();
-        }
-    });
+    let currentVideoTime = 0;
+    let obs1 = null;
+    let obs2 = null;
+    let obs3 = null;
+    let obs4 = null;
 
     /**
-     * MAIN FUNCTIONS
-     */
+    * MAIN FUNCTIONS
+    */
 
-    function main() {
-        const e = document.getElementById('movie_player');
-        if (!e) return;
+    const sourceCode = () => {
+        // check if exist
+        if (obs1) return;
+        // check for element
+        const condition1 = getElementByXpath('/html/body/ytd-app');
+        if (!condition1) return;
 
-        try {
-            // Create an observer instance
-            observer = new MutationObserver((mutations) => {
-                skipBtnClick();
-                adVideoManipulation();
-            });
-            observer.observe(e, {
-                attributes: true,
-                attributeFilter: ['class'],
-            });
+        obs1 = new MutationObserver(() => {
+            // check if exist
+            if (obs2) return;
+            // check for element
+            const condition2 = getElementByXpath('//*[@id="page-manager"]');
+            if (!condition2) return;
 
-            adVideoManipulation();
-            skipBtnClick();
-        } catch (err) {
-            console.error(err);
-        }
-    }
+            obs2 = new MutationObserver(() => {
+                // check if exist
+                if (obs3) return;
+                // check for element
+                const condition3 = getElementByXpath('//*[@id="page-manager"]/ytd-watch-flexy');
+                if (!condition3) return;
 
-    function enforcementListener() {
-        const xpath = '/html/body/ytd-app';
-        const e = getElementByXpath(xpath);
-        if (!e) {
-            console.info('enforcementListener failed');
-            return;
-        }
+                obs3 = new MutationObserver(() => {
+                    // check if exist
+                    if (obs4) return;
+                    // check for element
+                    const condition4 = getElementByXpath('//*[@id="ytd-player"]');
+                    if (!condition4) return;
 
-        try {
-            observer2 = new MutationObserver((mutations) => {
-                closeEnforcementMessage();
-            });
-            observer2.observe(e, {
-                subtree: true,
+                    // reload on ad blocker warnings
+                    refreshOnEnforcementMessage();
+                    obs4 = new MutationObserver(() => {
+                        skipBtnClick();
+                        adVideoManipulation();
+                        actualVideoListenser();
+                        closeEnforcementMessage();
+                        refreshOnEnforcementMessage();
+                    })
+                    obs4.observe(condition4, {
+                        childList: true,
+                        subtree: true,
+                    });
+                });
+                obs3.observe(condition3, {
+                    childList: true,
+                    subtree: true,
+                });
+            })
+            obs2.observe(condition2, {
                 childList: true,
-                attributes: true,
-                attributeFilter: ['class', 'style'],
-                characterData: true,
+                subtree: true,
             });
-
-            closeEnforcementMessage();
-        } catch (err) {
-            console.error(err);
-        }
+        })
+        obs1.observe(condition1, {
+            childList: true,
+            subtree: true,
+        });
     }
 
     /**
-     * HELPER FUNCTIONS
-     */
+    * HELPER FUNCTIONS
+    */
 
-    const getElementByXpath = (path) => document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    // const getAllElementsByRegex = () => {
+    //     const regex = /skip-button:\d+/; // Example: matches "skip-button:10", "skip-button:1p", etc.
+    //     const elements = document.querySelectorAll('[id]');
+    //     elements.forEach(element => {
+    //         if (regex.test(element.id)) {
+    //             element.click();
+    //             console.log("skip button click by Regex successful");
+    //         }
+    //     });
+    // }
 
-    function skipBtnClick() {
+    const getElementByXpath = (path) => {
+        return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    }
+
+    const skipBtnClick = () => {
         try {
             const skipBtn1 = getElementByXpath('//span[@class="ytp-ad-skip-button-container"]/button');
             if (skipBtn1) {
                 skipBtn1.click();
                 console.info('skip button click by XPath successful');
-                incrementAdSkipCount();
             }
+            
+            // getAllElementsByRegex();
+            
             const skipBtnList = [];
             const targetClassNames = [
                 "ytp-ad-skip-button-modern",
@@ -108,7 +119,6 @@
                 if (btn) {
                     btn.click();
                     console.info('skip button click by ClassName/ID successful');
-                    incrementAdSkipCount();
                 }
             });
         } catch (err) {
@@ -116,65 +126,73 @@
         }
     }
 
-    function adVideoManipulation() {
-        try {
-            const video = getElementByXpath('//*[@id="movie_player" and contains(@class, "ad-showing")]/div[1]/video');
-            if (video) {
-                // video.removeEventListener('ratechange', () => { console.log('ratechange listener removed'); });
-                // video.removeEventListener('seeking', () => { console.log('seeking listener removed'); });
-                // video.removeEventListener('timeupdate', () => { console.log('timeupdate listener removed'); });
-                // video.removeEventListener('seeked', () => { console.log('seeked listener removed'); });
-                video.volume = 0;
-                video.muted = true;
-                video.playbackRate = playbackRate;
-                // incrementAdSkipCount();
-                // if (video.currentTime && video.duration) {
-                //     video.currentTime = video.duration;
-                // }
-                incrementAdSkipCount();
-            }
-        } catch (err) {
-            console.error(err);
+    const adVideoManipulation = () => {
+        setTimeout(() => {
+            const videoElement = getElementByXpath('//*[@id="movie_player" and contains(@class, "ad-showing")]/div[1]/video');
+            if (!videoElement) return;
+            videoElement.volume = 0;
+            videoElement.muted = true;
+            videoElement.playbackRate = playbackRate;
+        }, 500);
+    }
+
+    const actualVideoListenser = () => {
+        setTimeout(() => {
+            const videoElement = getElementByXpath('//*[@id="movie_player" and not(contains(@class, "ad-showing"))]/div[1]/video');
+            if (!videoElement) return;
+            videoElement.addEventListener("timeupdate", () => {
+                if (!!parseInt(videoElement.currentTime)) {
+                    currentVideoTime = parseInt(videoElement.currentTime);
+                }
+            });
+        }, 500);
+    }
+
+    const closeEnforcementMessage = () => {
+        setTimeout(() => {
+            const adElement = getElementByXpath('//*[@id="container" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="header" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="dismiss-button" and contains(@class, "ytd-enforcement-message-view-model")]/button-view-model/button');
+            if (!adElement) return;
+            adElement.click();
+        }, 500);
+    }
+
+    const refreshOnEnforcementMessage = () => {
+        const adElement = getElementByXpath('//*[@id="container" and contains(@class, "ytd-enforcement-message-view-model")]');
+        if (!adElement) return;
+
+        const currentURL = window.location.href ?? document.URL;
+        const timestamp = currentVideoTime ?? 0;
+        if (currentURL && timestamp) {
+            let url = new URL(currentURL);
+            let params = new URLSearchParams(url.search);
+            params.set("t", `${timestamp}s`);
+            let newURL = new URL(`${url.origin}${url.pathname}?${params}`);
+            window.location.href = newURL;
+        } else {
+            window.location.reload();
         }
     }
 
-    function closeEnforcementMessage() {
-        try {
-            const elementXPath = '//*[@id="container" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="header" and contains(@class, "ytd-enforcement-message-view-model")]//*[@id="dismiss-button" and contains(@class, "ytd-enforcement-message-view-model")]/button-view-model/button';
-            const adElement = getElementByXpath(elementXPath);
-            if (adElement) {
-                adElement.click();
-                console.info('enforcement button click by XPath successful');
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
+    const resetCurrentVideoTime = () => { if (currentVideoTime) currentVideoTime = 0; }
 
-    function incrementAdSkipCount() {
-        adSkipCount++;
-        chrome.runtime.sendMessage({ action: "incrementAdSkipCount", count: adSkipCount });
-    }
+    /**
+    * INIT FUNCTIONS
+    */
 
-    window.addEventListener('beforeunload', unloadAllObservers);
+    // Initial setup and check
+    window.addEventListener("load", () => {
+        sourceCode();
+    })
 
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.action === "disconnectObserver") {
-            unloadAllObservers();
-        }
-    });
+    document.addEventListener("load", () => {
+        sourceCode();
+    })
 
-    function unloadAllObservers() {
-        if (observer) {
-            observer.disconnect();
-            observer = null;
-            console.info('observer disconnected');
-        }
-        if (observer2) {
-            observer2.disconnect();
-            observer2 = null;
-            console.info('observer2 disconnected');
-        }
-    }
+
+    window.addEventListener('locationchange', resetCurrentVideoTime);
+
+    window.addEventListener('popstate', resetCurrentVideoTime);
+
+
 
 })();
